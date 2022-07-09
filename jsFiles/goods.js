80d685010fc3e653d0118ff38f7c96228c041f85
Alina <a.08pon@gmail.com>
2022-07-09 14:00:03 +0100

    js fixed

--- jsFiles/goods.js
+++ jsFiles/goods.js
@@ -16,6 +16,9 @@ class Counter {
         incr();
       } else if (target.classList.contains("decrease")) {
         function decr() {
+          if (counterScore < 0) {
+            return (counterScore = 0);
+          }
           counterScore--;
         }
         decr();
